e105dfbfc36a4baf86f23dd97a20d39934bbf811
Suleng18 <90331457+suleng18@users.noreply.github.com>
2022-11-22 15:25:49 +0700

    vid70-useMemo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import Content from './Content';
 
 function App() {
-  const [count, setCount] = useState(60);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [products, setProducts] = useState([]);
+  const nameRef = useRef();
 
-  const timerId = useRef();
-  const prevCount = useRef();
+  const handleSubmit = () => {
+    setProducts([...products, { name, price: +price }]);
 
-  useEffect(() => {
-    prevCount.current = count;
-  }, [count]);
-
-  const handleStart = () => {
-    timerId.current = setInterval(() => {
-      setCount((prevCount) => prevCount - 1);
-    }, 1000);
+    setName('');
+    setPrice('');
+    nameRef.current.focus();
   };
 
-  const handleStop = () => {
-    clearInterval(timerId.current);
-  };
-
-  console.log(count, prevCount.current);
+  const total = useMemo(() => {
+    const result = products.reduce((result, product) => {
+      console.log('tinh toan lai');
+      return result + product.price;
+    }, 0);
+    return result;
+  }, [products]);
 
   return (
     <div style={{ padding: '32px' }}>
-      <h1>{count}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <input
+        ref={nameRef}
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input placeholder="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
